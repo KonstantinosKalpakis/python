755ee5fd13f7cd125092e712295d5bbd03fccde3
kalpax <kon.kalpakis@gmail.com>
2021-12-19 23:52:14 +0200

    new_exercise

---
+++ ex_001.py
@@ -0,0 +1,6 @@
+x = input("Give radius: ")
+radius = float(x)
+perimeter = 2*3.14*radius
+surface = 3.14*radius**2
+print("The perimeter of the circle is: ", perimeter)
+print("The perimeter of the circle is: ", surface)
